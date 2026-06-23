47fcebc942af3b3f8db00992a891dff4fb0b30de
vasanth <vasanthmarriboina@gmail.com>
2026-06-23 18:36:53 +0530

    Completed Arrays Task

--- script.js
+++ script.js
@@ -16,38 +16,36 @@ let flatArray = [1, [2, [3, [4, [5]]]]];
 console.log(flattenDeep(flatArray));
 
 
-//Spiral Matrix Printing.
-
-function spiralMatrix(n){
-    let matrix= Array.from({ length : n},()=>Array(n));
-    let top = 0;
-    let bottom = n-1;
-    let left = 0;
-    let right = n-1;
-    let num = 1;
-    while(top <= bottom && left <= right){
-        for(let i = left;i<=right;i++){
-            matrix[top][i] = num++;
-        }
-        top++;
-         for (let i = top; i <= bottom; i++) {
-            matrix[i][right] = num++;
-        }
-        right--;
-         for (let i = right; i >= left; i--) {
-            matrix[bottom][i] = num++;
-        }
-        bottom--;
-         for (let i = bottom; i >= top; i--) {
-            matrix[i][left] = num++;
-        }
-        left++;
-
-
-    }
-    return matrix;
+//Spiral Matrix .....
+function spiral(n){
+let num=1;
+const matrix = [];
+for(let i=0;i<n;i++){
+    matrix.push(Array(n).fill(0));
 }
-console.log(spiralMatrix(3));
+
+let  top =0;
+let bottom = n-1;
+let left = 0
+let right = n-1;
+
+for(let i=left;i<=right;i++){
+    matrix[top][i] = num++;
+
+}top++;
+for(let i = top;i<=bottom;i++){
+    matrix[i][right] = num++;
+}right--;
+for(let i = right;i>=left;i--){
+    matrix[bottom][i] = num++;
+}bottom--;
+for(let i = bottom;i>left;i--){
+    matrix[i][left] = num++;
+}left++;
+matrix[top][left] = num++;
+console.log(matrix);
+}
+spiral(3);
 
 
 //Sequential async loop (no Promise.all).
@@ -73,7 +71,7 @@ async function fetchUsers() {
 fetchUsers();
 
 
-
+//Arrays GroupBy
 function groupBy(arr, key) {
     const result = {};
 
@@ -96,3 +94,108 @@ const people = [
 ];
 
 console.log(groupBy(people, "city"));
+
+
+// Implement Array.prototype.reduce() from scratch
+
+Array.prototype.myReduce = function(fn,init){
+    const arr = this;
+    if(typeof fn !== "function"){
+        throw new TypeError(
+            "Callback must be a function"
+        );
+    }
+    if(arr.length === 0 && arguments.length < 2){
+        throw new TypeError(
+            "Reduce of empty array with no initial value"
+        );
+    }
+    let accumulator;
+    let startIndex;
+    if(arguments.length >1){
+        accumulator = init;
+        startIndex = 0;
+    }else{
+        startIndex = 0;
+        while
+            (startIndex < arr.length && !(startIndex in arr)){
+                startIndex++;
+            }
+            if(startIndex >= arr.length){
+                throw new TypeError("Reduce of empty array with no initial value");
+            }
+            accumulator = arr[startIndex];
+            startIndex++;
+    }
+    for(let i = startIndex;i<arr.length;i++){
+        if(!(i in arr))continue;
+        accumulator = fn(accumulator,arr[i],i,arr);
+    }
+    return accumulator;
+    };
+//Test 1 With initial Value 0
+    let result = [1,2,3].myReduce((acc,x)=>acc+x,0);
+    console.log(result);
+    // Test 2 without initial value
+    let res = [1,2,3,].myReduce((acc,curr)=>acc+curr);
+    console.log(res);
+    // Empty Array With Initial Value
+    let value = [].myReduce((acc,cur)=>{acc+cur},0);
+    console.log(value);
+    //Empty Array Without Initial Value
+    // let val = [].myReduce((acc,cur)=>{acc+cur});
+    // console.log(val);
+
+    //Sparse Array
+const arr = [1, , , 4];
+
+console.log(
+    arr.myReduce((acc,x)=>acc+x)
+);
+let ar = [1,,,4];
+console.log(ar.myReduce((acc,cur)=>acc+cur));
+
+
+
+
+//SubArray Sum O(n2) notation
+function subarraySum(array,k)
+{
+    for(let i=0;i<array.length;i++){
+        let sum = 0;
+        for(let j=i;j<array.length;j++){
+            sum += array[j];
+        if(sum===k){
+            console.log(array.slice
+                (i,j+1)
+            );
+        }
+    }
+    }
+}
+let arra = [1, 2, 3, 0, 3, 3];
+subarraySum(arra,3);
+
+
+
+// O(n) Solution Using Prefix Sum Map
+function subArraySum(arr,k){
+    const result = [];
+    const map = new Map();
+    map.set(0,[-1]);
+    let prefixSum = 0;
+    for(let i=0;i<arr.length;i++){
+        prefixSum += arr[i];
+        const needed = prefixSum-k;
+        if(map.has(needed)){
+            for(const start of map.get(needed)){
+                result.push(arr.slice(start+1,i+1));
+            }
+        }
+        if(!map.has(prefixSum)){
+            map.set(prefixSum,[]);
+        }
+        map.get(prefixSum).push(i);
+    }return result
+}
+console.log(subArraySum([1,2,3,0,3],3));
